03c3bf1faa2cbd6ed215305ddc0916e20737e3ee
Adam Hinton <adam.d.hinton@gmail.com>
2021-11-05 13:20:08 -0400

    done with interview questions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import {React, useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
+
+
 
 const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+  
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setCharacterList(res.data)
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -10,10 +29,18 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
+(    <div className="App">
       <h1 className="Header">Characters</h1>
-    </div>
+      {
+        characterList.map(item=> {
+          return <Character info = {item}/>
+        })
+      }
+    </div>)
   );
 }
 
 export default App;
+
+
+//Use keys if you’re looping/mapping over data. key={fr.id} for instance for looping over fr.
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import styled from 'styled-components';
+import React from 'react';
+
+const StyledDiv = styled.div `
+h1{
+    color: blue;
+}
+`
+
+
+function Character ({info}){
+    return (
+    <StyledDiv>
+  <h1>Character name: {info.name}</h1>
+    </StyledDiv>
+    )
+}
+
+export default Character;
